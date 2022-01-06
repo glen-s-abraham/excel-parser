5d951ced26bb52bbcb39b9ba3413c7404749a4b8
iss211 <glen.abraham@immcossi.com>
2022-01-06 18:39:59 +0530

    column trimmer logic added

--- src/main/java/com/glen/ExcelParser/App.java
+++ src/main/java/com/glen/ExcelParser/App.java
@@ -17,9 +17,9 @@ public class App
     public static void main( String[] args )
     {
         String path="C:/Users/Glen/Fulton-Hogan/WorkoutFiles/OneDrive_1_1-4-2022/TLS10.1.2_FIR_5GUG-20_N50_1.1.xlsx";
-        //excelParser.loadExcelFiles(path);
+        excelParser.loadExcelFiles(path);
         String mapInfoPath="C:/Users/Glen/Fulton-Hogan/WorkoutFiles/OneDrive_1_1-4-2022/5GUG-20_IDD_S18_1.1.zip";
         String compressPath="C:/Users/Glen/Fulton-Hogan/WorkoutFiles/uploadTest/";
-        System.out.println(FileUnzipper.unzip(mapInfoPath));
+        //System.out.println(FileUnzipper.unzip(mapInfoPath));
     }
 }
--- src/main/java/com/glen/ExcelParser/services/ExcelFileParserService.java
+++ src/main/java/com/glen/ExcelParser/services/ExcelFileParserService.java
@@ -2,6 +2,7 @@ package com.glen.ExcelParser.services;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,31 +21,46 @@ import org.apache.poi.openxml4j.opc.OPCPackage;
  */
 
 public class ExcelFileParserService {
-
+	
+	private String[] COLS_TO_INCLUDE= {
+			"FIR Pit Reference Number",
+			"latitude",
+			"longitude",
+			"nbn_system_identifier",
+			"FIR Duct Reference Number",
+			"material",
+			"ownership",
+			"LIC Reference Number",
+			"Ref_ID",
+			"MPS_ID"
+	};
+	
 	public void loadExcelFiles(String path) {
 		//Generate file stream with path
 		//POIFSFileSystem file = new POIFSFileSystem(new File(path));
+		
+		
 		OPCPackage pkg;
 		XSSFWorkbook workbook;
 		XSSFSheet sheet;
 		try {
 			pkg = OPCPackage.open(new File(path));
 			workbook  = new XSSFWorkbook(pkg);
-			sheet =  workbook.getSheetAt(3);
+			sheet =  workbook.getSheetAt(4);
 		
 			//iterateThroughSheet(sheet);
 			
 			List<Row> sheetRows = getRowListFromSheet(sheet);
 			List<Row> trimmedRows = removeEmptyRows(sheetRows);
+			List<Integer> colIndexesToInclude = getColumnIndecesToInclude(trimmedRows);
+			trimmedRows = dropColumnsExcept(trimmedRows, colIndexesToInclude);
 			List<String> columnHeaders = getCellValuesFromRow(trimmedRows.get(0));
-			
+			System.out.println(columnHeaders);
 			SimpleInsertQuery defaultQuery = new SimpleInsertQuery.Builder("fir_table", columnHeaders).build();
 			SimpleInsertQuery queryWithValue = new SimpleInsertQuery.Builder("fir_table", columnHeaders)
-					.setValues(getCellValuesFromRow(trimmedRows.get(1))).build();
-			
+					.setValues(getCellValuesFromRow(trimmedRows.get(2))).build();
 			System.out.println(defaultQuery.getSqlQuery());
 			System.out.println(queryWithValue.getSqlQuery());
-			
 			
 		}catch (Exception e) {
 			e.printStackTrace();
@@ -53,6 +69,34 @@ public class ExcelFileParserService {
 		
 	}
 	
+	private List<Row> dropColumnsExcept(List<Row> sheetRows,List<Integer> indecesToInclude) {
+		List<Row> rowList = sheetRows;
+		int totColumns = rowList.get(0).getPhysicalNumberOfCells();
+		for(int i=0;i<totColumns;i++) {
+			if(indecesToInclude.contains(i))
+				continue;
+			for(Row row:rowList) {
+				if(row.getCell(i)!=null)
+					row.removeCell(row.getCell(i));
+			}
+			
+		}
+		return rowList.size()==0?null:rowList;
+	}
+
+	//Need refactoring
+	private List<Integer> getColumnIndecesToInclude(List<Row> sheetRows) {
+		List<Integer> requiredIndeces = new ArrayList<>();
+		for(Row row:sheetRows) {	
+			List<String> cellValues = getCellValuesFromRow(row);
+			for(int i=0;i<cellValues.size();i++) {
+				if(Arrays.asList(COLS_TO_INCLUDE).contains(cellValues.get(i))) 
+					requiredIndeces.add(i);		
+			}		
+		}
+		return requiredIndeces.size()==0?null:requiredIndeces;
+	}
+
 	//preprocessing step 1 remove empty rows
 	private List<Row> removeEmptyRows(List<Row> sheetRows) {
 		List<Row> rowList = new ArrayList<Row>();
